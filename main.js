03ed3ff5e99a28a830599e0fca79c3df4933379c
aurumrao <109160817+aurumrao@users.noreply.github.com>
2024-03-08 11:20:45 -0600

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,8 @@
+noseX=0;
+noseY=0;
+difference=0;
+leftWristX=0;
+rightWristX=0;
 function setup () {
     video = createCapture(VIDEO);
     video.size(530, 400);
@@ -14,10 +19,23 @@ function modelLoaded(){
 
 }
 function draw() {
-    background('#B9EBFF')
+    document.getElementById("side length").innerHTML = "The current side length is " + difference + " px";
+    background('#B9EBFF');
+    fill('#004E75');
+    stroke('#004E75');
+    square(noseX, noseY, difference);
+
 }
 function gotPoses(results) {
     if(results.length > 0){
         console.log(results);
+        noseX = results[0].pose.nose.x;
+        noseY = results[0].pose.nose.y;
+        console.log("Nose X = " + noseX + ", Nose Y = " + noseY);
+
+        leftWristX = results[0].pose.leftWrist.x;
+        rightWristX = results[0].pose.rightWrist.x;
+        difference = floor(leftWristX - rightWristX);
+        console.log("Right Wrist X = " + rightWristX + ", Left Wrist X = " + leftWristX + ", Difference = " + difference + ".");
     }
 }
